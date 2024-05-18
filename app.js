513af2bb1d17ed5a6f0e76d8ec63f58baacbe126
waiphyo-paing <waiphyopaing.dev@gmail.com>
2024-05-18 23:54:00 +0700

    Initial commit

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1851750858823d558aaa3e3d62af3077'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=1851750858823d558aaa3e3d62af3077&query="'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1851750858823d558aaa3e3d62af3077';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=1851750858823d558aaa3e3d62af3077&query="';
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-const main = document.getElementById('main')
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
 
-getMovie(API_URL)
+getMovie(API_URL);
 
 async function getMovie(url){
-     const res = await fetch(url)
-     const data = await res.json()
+     const res = await fetch(url);
+     const data = await res.json();
 
-     console.log(data.results)
-     showMovieList(data.results)
+     console.log(data.results);
+     showMovieList(data.results);
 }
 
 function showMovieList(movies){
@@ -22,8 +22,8 @@ function showMovieList(movies){
      movies.forEach((movie) => {
           const { overview, title, vote_average, poster_path } = movie;
 
-          const movieEl = document.createElement('div')
-          movieEl.classList.add('movie')
+          const movieEl = document.createElement('div');
+          movieEl.classList.add('movie');
 
           movieEl.innerHTML = `
           <img src="${IMG_PATH + poster_path}" alt="">
@@ -37,32 +37,32 @@ function showMovieList(movies){
                <h3>Overview</h3>
                ${overview}
           </div>
-          `
+          `;
 
-          main.appendChild(movieEl)
-     })
+          main.appendChild(movieEl);
+     });
 }
 
 function ratingText(vote){
      if(vote >= 8){
-          return 'green'
+          return 'green';
      }else if(vote >= 5){
-          return 'orange'
+          return 'orange';
      }else{
-          return 'red'
+          return 'red';
      }
 }
 
 form.addEventListener('submit', (e) => {
-     e.preventDefault()
+     e.preventDefault();
 
-     const searchTerm = search.value
+     const searchTerm = search.value;
 
      if(searchTerm && searchTerm !== ''){
-          getMovie(SEARCH_API + searchTerm)
+          getMovie(SEARCH_API + searchTerm);
 
-          search.value = ''
+          search.value = '';
      }else{
-          window.location.reload()
+          window.location.reload();
      }
 })
