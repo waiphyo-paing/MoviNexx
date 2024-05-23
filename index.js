7a990b4e98c24124a0ede6146c991436c6c7ad8c
waiphyo-paing <waiphyopaing.dev@gmail.com>
2024-05-23 22:54:54 +0700

    setup Api

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from "express";
+import { configDotenv } from "dotenv";
+import axios from "axios";
+
+configDotenv();
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-     res.render("index.ejs");
+const API_URL = "https://api.themoviedb.org/3/discover/movie";
+const config = {
+     headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer ' + process.env.MOVIE_DB_ACCESS_TOKEN
+     }
+};
+
+app.get('/', async (req, res) => {
+     try{
+          const request = await axios.get(API_URL, config);
+          const response = request.data;
+          res.render("index.ejs");
+     }catch(error){
+          console.log(error);
+     }
 });
 
 app.listen(PORT, () => {
